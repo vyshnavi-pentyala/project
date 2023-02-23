016d2e509b62eeda864144211c1919930d0f7516
maddy <maddy@makeskilled.com>
2023-02-23 11:20:53 +0530

    updating db connections

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 from flask import Flask,render_template,redirect,request,session
+import mysql.connector as mysql
+import json
+
+app=Flask(__name__)
+db=mysql.connect(
+    host='localhost',
+    user='root',
+    password='',
+    database='project17'
+)
+
+cur=db.cursor()
 
 # Create APP to launch Web Server
 app=Flask(__name__)
@@ -14,7 +26,14 @@ def signupform():
     username=request.form['username']
     password=request.form['password']
     print(username,password)
-    return render_template('index.html',res='Registered Successfully')
+    try:
+        sql='INSERT INTO register (username,password) VALUES (%s,%s)'
+        values=(username,password)
+        cur.execute(sql,values)
+        db.commit()
+        return render_template('index.html',res='Registered Successfully')
+    except:
+        return render_template('index.html',err='Register Failed')
 
 # Create a Route for Login Form
 @app.route('/loginform',methods=['post'])
@@ -22,6 +41,14 @@ def loginform():
     username=request.form['username1']
     password=request.form['password1']
     print(username,password)
+
+    sql='select * from register'
+    cur.execute(sql)
+    result=cur.fetchall()
+    data=[]
+    for i in result:
+        data.append(i)
+    print (json.dumps(data))
     return render_template('index.html',res='Login Valid')
 
 
