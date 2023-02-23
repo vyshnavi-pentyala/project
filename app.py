6c05d4abdfd9daabf8c59001b1880d14b12c26a0
maddy <maddy@makeskilled.com>
2023-02-23 12:06:44 +0530

    updating route for upload file

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask,render_template,redirect,request,session
 import mysql.connector as mysql
-import json
+import os
+from werkzeug.utils import secure_filename
+
+
 
 db=mysql.connect(
     host='localhost',
@@ -63,6 +66,26 @@ def loginform():
 @app.route('/dashboard')
 def dashboardPage():
     return render_template('dashboard.html')
+
+# Create a Route for Upload
+@app.route('/upload')
+def uploadPage():
+    return render_template('upload.html')
+
+# Create a Route for Upload File
+@app.route('/uploadFile',methods=['post'])
+def uploadFile():
+    doc=request.files['chooseFile']
+    if session['username'] not in os.listdir():
+        os.mkdir(session['username'])
+    doc1=secure_filename(doc.filename)
+    doc.save(session['username']+'/'+doc1)
+    return render_template('upload.html',res='File Uploaded')
+
+# Create a Route for Sender
+@app.route('/sender')
+def senderPage():
+    return render_template('sender.html')
 
 if (__name__=="__main__"):
     app.run(debug=True,port=5001)
